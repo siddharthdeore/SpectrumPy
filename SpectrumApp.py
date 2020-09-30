194360f97c29ddf54270eecb2725c8cf4d031014
Siddharth Deore <siddharthdeore@gmail.com>
2020-10-01 00:26:51 +0200

    UI/UX Minor Changes.

--- SpectrumApp.py
+++ SpectrumApp.py
@@ -18,8 +18,10 @@ from matplotlib import mlab as mlab
 
 # Supported gain values (29): 0.0 0.9 1.4 2.7 3.7 7.7 8.7 12.5 14.4 15.7 16.6 19.7 20.7 22.9 25.4 28.0 29.7 32.8 33.8 36.4 37.2 38.6 40.2 42.1 43.4 43.9 44.5 48.0 49.6
 pg.setConfigOptions(antialias=True)
+#pg.setConfigOption('background', '#f0f0f0')
 pg.setConfigOption('background', '#000000')
-pg.setConfigOption('foreground', '#aaaaaa')
+pg.setConfigOption('foreground', '#a0a0a0')
+
 class SDRThread(QThread):
     signal = pyqtSignal(object)
     def __init__(self,sample_rate = 2.4e6,center_freq = 100.0e6,freq_correction = 60, gain = 33.8, chunks = 1024):
@@ -87,8 +89,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
         #self.PSD = np.arange(-1,2)
         #self.freq = np.arange(-1,2)*self.band_width/2.0 + self.center_freq
-        self.freq = np.linspace(-self.band_width/2,self.band_width/2,self.CHUNK) + self.center_freq
-        self.data = np.random.rand(self.CHUNK)*0.000001
+        self.calc_freq = lambda bw,nfft,cf: np.linspace(-bw/2,bw/2,nfft) + cf
+
+        self.freq = self.calc_freq(self.band_width,self.CHUNK,self.center_freq)
+
+        self.data = np.random.rand(self.CHUNK)*1e-6
         self.history = np.zeros(shape=(self.history_length, self.CHUNK))
 
         self.waterfallView.plot()
@@ -114,7 +119,6 @@ class MainWindow(QtWidgets.QMainWindow):
                                             fillBrush = (55,155,255,100),
                                             padding = 0.0,
                                             autoRange=False)
-
         self.curve.setPen((255,255,195,90), width=1)
         self.curvePeaks = self.spectrumView.plot(self.freq,self.freq*1.0e-5, pen=None, symbol='d',symbolBrush=(215,155,255,200))
         self.spectrumView.setLabel("left", "Power", units="dB")
@@ -127,15 +131,16 @@ class MainWindow(QtWidgets.QMainWindow):
          # Create crosshair
         self.vLine = pg.InfiniteLine(angle=90, movable=False, pen='#aaaabb')
         self.vLine.setZValue(1000)
-
         self.hLine = pg.InfiniteLine(angle=0, movable=False, pen='#aaaabb')
         self.hLine.setZValue(1000)
-
         self.spectrumView.addItem(self.vLine, ignoreBounds=True)
         self.spectrumView.addItem(self.hLine, ignoreBounds=True)
         self.vLine2 = pg.InfiniteLine(angle=90, movable=False, pen='#aaaabb')
         self.vLine2.setZValue(1000)
         self.waterfallView.addItem(self.vLine2,ignoreBounds=True)
+
+        self.spectrumView.setTitle('Power Spectral Density')
+
         # Link ranges and focus of spectrum and waterfall
         self.spectrumView.setXLink(self.waterfallView)
         # Gradient color level selector
@@ -148,8 +153,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.gradient_presets = ['thermal', 'flame', 'yellowy', 'bipolar', 'spectrum', 'cyclic', 'greyclip', 'grey']
         self.histogram.gradient.loadPreset(self.gradient_presets[1])
         #self.histogram.setHistogramRange(-50, 0)
-        self.histogram.setLevels(1.0e-4, 0.008)
-        self.histogram.setHistogramRange(1.0e-4, 1.0e-2, padding=0.1)
+        self.histogram.setLevels(-1.0e-3, 1.0e-2)
+        self.histogram.setHistogramRange(-2.0e-2, 2.0e-3, padding=1)
         
         # Lables
         self.lblFreq.setText(str(self.center_freq))
@@ -166,17 +171,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.PPM = self.spinBoxPPM.value()
         self.gain = float(self.comboBoxGain.currentText())
         self.CHUNK = int(self.comboBoxNFFT.currentText())
-        self.freq = np.linspace(-self.band_width/2,self.band_width/2,self.CHUNK) + self.center_freq
+        self.freq = self.calc_freq(self.band_width,self.CHUNK,self.center_freq)
         self.data = np.random.rand(self.CHUNK)*0.000001
         self.history = np.zeros(shape=(self.history_length, self.CHUNK))
 
         self.spectrumView.setLimits(yMin=-4,yMax=-1)
         self.waterfallView.setLimits(xMin=self.freq[0],xMax=self.freq[-1],yMin=-self.history_length,yMax=0)
 
-        new_ratio =(self.freq[-1] - self.freq[0]) / self.CHUNK
-
-        self.img_waterfall.scale(new_ratio/self.aspect_ratio, 1)
-        self.aspect_ratio = (self.freq[-1] - self.freq[0]) / self.CHUNK
+        calc_ratio =lambda : (self.freq[-1] - self.freq[0]) / self.CHUNK
+        self.img_waterfall.scale(calc_ratio()/self.aspect_ratio, 1)
+        self.aspect_ratio = calc_ratio()
 
         self.histogram.setImageItem(self.img_waterfall)
 
@@ -215,7 +219,7 @@ class MainWindow(QtWidgets.QMainWindow):
             self.spinBoxFrequency.valueChanged.connect(self.sdrSetFrequency)        
             # GUI Timer Threads for plots 
             self.render_timer = QTimer()
-            self.render_timer.setInterval(50)
+            self.render_timer.setInterval(40)
             self.render_timer.timeout.connect(self.render)
             self.render_timer.start()
 
@@ -249,12 +253,6 @@ class MainWindow(QtWidgets.QMainWindow):
             self.curvePeaks.setData(self.freq[idx],self.data[idx])
             self.spectrumView.enableAutoRange(axis='y', enable=False)
 
-
-            # scale waterfall image only once
-            #if(self.counter==1):
-            #        self.img_waterfall.scale((self.freq[-1] - self.freq[0]) / self.CHUNK, 1)
-            #        self.histogram.setImageItem(self.img_waterfall)
-
             self.img_waterfall.setImage(self.history.T,levels=self.histogram.getLevels())
             self.img_waterfall.setPos(self.freq[0],-self.history_length)
 
@@ -264,7 +262,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def sdrSetFrequency(self):
         if self.isRunning:
             self.center_freq = self.spinBoxFrequency.value()
-            self.freq = np.linspace(-self.band_width/2,self.band_width/2,self.CHUNK) + self.center_freq
+            self.freq = self.calc_freq(self.band_width,self.CHUNK,self.center_freq)
             self.spectrumView.setLimits(xMin=self.freq[0],xMax=self.freq[-1])
             self.waterfallView.setLimits(xMin=self.freq[0],xMax=self.freq[-1])
             
